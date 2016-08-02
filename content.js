90b2d7c34d3608991b49d110f500f244778a697f
Vicky Gray <vicky@buzzmyvideos.com>
2016-08-02 14:26:48 +0100

    Added background script - function sends a message to the active tab once browser action clicked - code in content script moved into a listener - now only executes once message received

--- content.js
+++ content.js
@@ -1,3 +1,12 @@
-var firstHref = $("a[href^='http']").eq(0).attr("href");
+// Listens for the message that is sent when user clicks the button,
+// and executes code once it receives it
 
-console.log(firstHref);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.message = "clicked_browser_action") {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+    }
+  }
+);
